51c8b974162aa381365f6359e86c49690b8df3bf
Fabio Setti <fabio.setti45@gmail.com>
2025-06-20 19:29:45 -0400

    Sampling distribution example

--- app.R
+++ app.R
@@ -3,85 +3,96 @@ library(munsell)
 library(ggplot2)
 library(bslib)
 
-
-information <- function(a, theta, df, sigma, b, d, q, lb, ub) {
-  
-  # Precompute reused terms
-  sqrt_nu <- sqrt(df)
-  exp_a_theta <- exp(a * theta)
-  exp_2a_theta <- exp(2 * a * theta)
-  
-  # Define terms
-  x1 <- b - lb + d * q
-  x2 <- b + d * q - ub
-  
-  num1 <- exp_a_theta * x1
-  denom1 <- exp_2a_theta * x1^2 + df * sigma^2
-  
-  num2 <- exp_a_theta * x2
-  denom2 <- exp_2a_theta * x2^2 + df * sigma^2
-  
-  atan_term1 <- atan(num1 / (sqrt_nu * sigma))
-  atan_term2 <- atan(num2 / (sqrt_nu * sigma))
-  
-  # Final expression
-  prefactor <- a^2 * exp(-a * theta) * df * (1 + df) * sigma^2
-  
-  result <- prefactor * (
-    -num1 / denom1 + 
-      num2 / denom2 + 
-      atan_term1 / (sqrt_nu * sigma) - 
-      atan_term2 / (sqrt_nu * sigma)
-  )
-  
-  return(result)
-}
-
-
 theme_set(theme_classic(base_size = 16, 
                         base_family = 'serif'))
 
-# Define the UI 
+
+
 ui <- bslib::page_fluid(
   # Main plot area
   
   mainPanel(
     
-    fluidRow(column(12, plotOutput("distPlot", height = "370px", width = "150%")))),
+    fluidRow(column(6, plotOutput("Population")),
+             column(6, plotOutput("Sample")))),
   
   fluidRow(
-    column(2, numericInput("b", "b", value = 0, min = -10, max = 10, step = 0.25)),
-    column(2, numericInput("d", "d", value = 1, min = 0.1, max = 10, step = 0.25)),
-    column(2, numericInput("sigma", "sigma", value = 1, min = 0.1, max = 5, step = 0.25)),
-    column(2, numericInput("a", "a", value = 1, min = -3, max = 3, step = 0.25)),
-    column(2, numericInput("q", "q", value = 0, min = -2, max = 2, step = 1)),
-    column(2, numericInput("df", "df", value = 5, min = 0.1, max = 200, step = 1)))
+    column(2, numericInput("Mean", "\u03BC", value = 5, min = -100, max = 100, step = 1)),
+    column(2, numericInput("SD", "\u03C3", value = 3, min = 0.1, max = 10, step = 0.25)),
+    column(2, numericInput("SS", "Sample Size", value = 100, min = 2, max = 20000, step = 10)),
+    column(2, numericInput("NS", "Experiments", value = 10, min = 1, max = 20000, step = 10))
+    , column(2, actionButton("start", "Run Experiments"))
+  )
 )
 
 
 
 server <- function(input, output, session) {
   
-  output$distPlot <- renderPlot({
+  output$Population <- renderPlot({
     # Get values from inputs
     
     
     
     # Create the plot
     ggplot() +
-      geom_function(fun = information, args = list(a = input$a, 
-                                                   df = input$df, 
-                                                   sigma = input$sigma, 
-                                                   b = input$b,
-                                                   d = input$d, 
-                                                   q = input$q, 
-                                                   lb = -3, 
-                                                   ub = 3), color = "#1b305c") +
-      xlab("\u03b8") +
-      ylab(expression(Epsilon~I*(theta))) +
-      xlim(-6, 6)
+      xlim(input$Mean-3*input$SD, input$Mean + 3*input$SD)+
+      geom_function(fun = dnorm, args = list(mean = input$Mean,
+                                             sd = input$SD),
+                    color = "#1b305c") +
+      xlab("Population Distribution") +
+      geom_segment(aes(x = input$Mean,
+                       xend = input$Mean,
+                       y = 0,
+                       yend = dnorm(input$Mean, mean = input$Mean, sd = input$SD)),
+                   lty = 2) +
+      scale_y_continuous(expand = c(0,0)) + 
+      theme( axis.text.y=element_blank(),
+             axis.ticks.y=element_blank(),
+             axis.line.y = element_blank(),
+             axis.title.y = element_blank())
     
     
+    
+    
+  }, res = 100)
+  
+  
+  
+  
+  
+  
+  sample <- eventReactive(input$start, {data.frame(colMeans(replicate(input$NS, 
+                                                                      rnorm(n = input$SS, 
+                                                                            mean = input$Mean, 
+                                                                            sd = input$SD))))})
+  
+  
+  
+  output$Sample <- renderPlot({
+    # Get values from inputs
+    
+    rand_sample <- sample()
+    
+    
+    colnames(rand_sample) <- "X"
+    
+    ggplot(rand_sample, aes(x = X)) +
+      xlab(paste("Distribution of Means for", nrow(rand_sample), "Experiments")) +
+      scale_y_continuous(expand = c(0,0)) +
+      geom_histogram(color = "black",
+                     linewidth = .8,
+                     fill = "#1b305c",
+                     bins = 30) +
+      annotate("text", 
+               x = Inf,
+               y = Inf,
+               label = paste0("Mean =", round(mean(rand_sample$X), 2),"\n",
+                              "SD (aka SE) =", round(sd(rand_sample$X), 2)), hjust=1,vjust = 1) + 
+      theme( axis.text.y=element_blank(),
+             axis.ticks.y=element_blank(),
+             axis.line.y = element_blank(),
+             axis.title.y = element_blank())
     
     
   }, res = 100)
